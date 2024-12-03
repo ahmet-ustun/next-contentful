63ae31558b606410897ca68f424f0d9961952438
Ahmet Ustun <austu@softserveinc.com>
2024-12-03 17:59:41 +0200

    Add RecipeCard component and integrate it into the Recipes page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { createClient } from "contentful";
 
+import RecipeCard from "../components/RecipeCard.js";
+
 export async function getStaticProps() {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -16,7 +18,11 @@ export async function getStaticProps() {
 }
 
 export default function Recipes({ recipes }) {
-  console.log(recipes);
-
-  return <div className="recipe-list">Recipe List</div>;
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
+    </div>
+  );
 }
