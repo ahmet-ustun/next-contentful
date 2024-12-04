f1bff5a9664dc6b9e747d163fd6decc769e80cf7
Ahmet Ustun <austu@softserveinc.com>
2024-12-04 16:20:57 +0200

    Replace loading state with Skeleton component in recipe details

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import Image from "next/image";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import Skeleton from "../../components/Skeleton.js";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -35,7 +36,7 @@ export async function getStaticProps({ params }) {
 }
 
 export default function RecipeDetails({ recipe }) {
-  if (!recipe) return <div>Loading...</div>;
+  if (!recipe) return <Skeleton />;
 
   const { featuredImage, title, cookingTime, ingredients, method } =
     recipe.fields;
